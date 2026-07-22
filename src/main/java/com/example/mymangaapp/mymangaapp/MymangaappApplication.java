3b66172dfa2f12015e1ca39d17b7677f2b0a2a42
HelloWorld2k5 <truongyukihara@gmail.com>
2026-07-22 12:14:03 +0700

    first commit

---
+++ src/main/java/com/example/mymangaapp/mymangaapp/MymangaappApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymangaapp.mymangaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymangaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymangaappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mymangaapp/mymangaapp/MymangaappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymangaapp.mymangaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymangaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
